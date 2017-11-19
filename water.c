492f7d5c99ba376bfab0cd097f61afb51586cd98
kasparovnutter <kasparovnutter@users.noreply.github.com>
2017-11-19 05:29:40 +0000

    20171119T052928Z

--- water.c
+++ water.c
@@ -1,35 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#import <cs50.h>
+#import <stdio.h>
 
-char bottles(char* a);
-int checkvalid(char* b);
+int main(void){
+    fprintf(stdout, "Minutes: ");
+    int time = get_int();
+    // gal = 128 oz
+    // bottle = 16 oz
+    // shower = 1.5 gal/min
+    // bottles = (total shower) / bottle
+    fprintf(stdout, "Bottles: %i\n", (time *= (1.5 * 128)) / 16);
 
-int main()
-{
-printf("Minutes:\n");
-char m = getchar();
-8
-checkvalid(&m); //return an exit code (int) from checkvalid
-char b = bottles(&m);
-printf("\033[2J");
-printf("Minutes:%c\n",m);
-printf("Bottles:%c\n",b);
-return 0;
-}
-
-char bottles(char* a) //*a is int m,
-{
-    int b = (*a) * 12; //b = bottles drank, char to input int literally (e.g '9' = 57)
-    return b;
-}
-
-int checkvalid(char* b)
-{
-    if ((int) *b >58 || (int) *b <47) //if m is ASCII letter, return 1
-    {
-    printf("\033[2J");
-    printf("enter a number\n");
-    return 1;
-    }
-return 0;
 }
